986d14b0185cb4d7ccc2bd62a1cb1b23aca39449
Martin Tang <martin.tang365@gmail.com>
2018-06-04 21:05:01 +0100

    First commit!

---
+++ app/src/main/java/com/martango/firstappnokotlin/DisplayMessageActivity.java
@@ -0,0 +1,19 @@
+package com.martango.firstappnokotlin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        // Get the string from the intent that started the activity.
+        String message = getIntent().getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+    }
+}
